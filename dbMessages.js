54df5b70a70da68bf0a228ab7ca119c4f2d8959e
Max Cohen <mdcohen1993@gmail.com>
2021-06-13 16:24:43 +0300

    back-end

---
+++ dbMessages.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+
+const whatsappSchema=mongoose.Schema({
+    message: String,
+    name: String,
+    timestamp: String,
+    received: Boolean
+});
+
+
+//collection
+module.exports = mongoose.model("messagecontent", whatsappSchema)
---
+++ server.js
@@ -0,0 +1,98 @@
+//imports
+const express = require("express");
+const mongoose = require("mongoose");
+const Messages = require("./dbMessages.js");
+const Pusher = require("pusher");
+const cors = require("cors");
+
+//config
+const app = express();
+const port = process.env.PORT || 9000;
+
+
+const pusher = new Pusher({
+  appId: "1218588",
+  key: "f1f3406db914393509ef",
+  secret: "d38fe888830bfd815d71",
+  cluster: "ap2",
+  useTLS: true
+});
+
+pusher.trigger("my-channel", "my-event", {
+  message: "hello world"
+});
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    next();
+})
+
+//DB config
+const dbPassword = "Hilleltorah1993!"
+const connection_url = "mongodb+srv://mdcohen1993:"+dbPassword+"@cluster0.clbnj.mongodb.net/whatsapp-clone-db?retryWrites=true&w=majority";
+
+mongoose.connect(connection_url, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+db.once("open", ()=>{
+    console.log("DB Connected");
+
+    const msgCollection = db.collection("messagecontent");
+    const changeStream = msgCollection.watch();
+
+    changeStream.on("change", change =>{
+        console.log("A change occured", change);
+
+        if (change.operationType === "insert") {
+            const messageDetails = change.fullDocument;
+            pusher.trigger("message", "inserted",
+            {
+                name: messageDetails.user,
+                message: messageDetails.message,
+                timestamp: messageDetails.timestamp
+            })
+        } else {
+            console.log("error triggering Pusher")
+        }
+    });
+});
+
+//api routes
+app.get('/', (req, res)=>res.status(200).send('hello world'));
+
+app.get("/messages/sync", (req,res)=>{
+    Messages.find((err, data) =>{
+        if(err) {
+            res.status(500).send(err)
+        } else{
+            res.status(200).send(data)
+        };
+    });
+});
+
+app.post("/messages/new", (req, res)=> {
+    const dbMessage = req.body;
+
+    Messages.create(dbMessage, (err, data) =>{
+        if(err) {
+            res.status(500).send(err)
+        }else{
+            res.status(201).send(data)
+        }
+    })
+});
+
+
+
+//app listening
+app.listen(port,()=>console.log(`Listening on localhost:${port}`));
